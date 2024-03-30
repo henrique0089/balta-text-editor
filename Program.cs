a532c145a73cd3a9ff5ec017be745c11b06ddc60
Carlos henrique <henriquemonteiro714@gmail.com>
2024-03-30 18:03:11 +0000

    done

--- Program.cs
+++ Program.cs
@@ -1,12 +1,90 @@
 ﻿using System;
+using System.IO;
 
-namespace dotnetcore
+namespace BaltaTextEditor
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Menu();
     }
+
+    static void Menu()
+    {
+      Console.Clear();
+
+      Console.WriteLine("O que você deseja fazer?");
+      Console.WriteLine("0 - Sair");
+      Console.WriteLine("1 - Abrir um arquivo");
+      Console.WriteLine("2 - Criar um novo arquivo");
+
+      short opt = short.Parse(Console.ReadLine());
+
+      switch (opt)
+      {
+        case 0:
+          Environment.Exit(0);
+          break;
+        case 1:
+          Open();
+          break;
+        case 2:
+          Edit();
+          break;
+        default:
+          Menu();
+          break;
+      }
+    }
+  
+    static void Open() 
+    {
+      Console.Clear();
+      Console.WriteLine("Qual o caminho do arquivo?");
+      string path = Console.ReadLine();
+
+      using (var file = new StreamReader(path))
+      {
+        string text = file.ReadToEnd();
+        Console.Write(text);
+      }
+
+      Console.WriteLine("");
+      Console.ReadLine();
+      Menu();
+    }
+
+    static void Edit() 
+    {
+      Console.Clear();
+
+      Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+      Console.WriteLine("-----------------------");
+      string text = "";
+
+      do
+      {
+        text += Console.ReadLine();
+        text += Environment.NewLine;
+      } while (Console.ReadKey().Key != ConsoleKey.Escape);
+     
+      Save(text);
+    }
+
+    static void Save(string text) {
+      Console.Clear();
+
+      Console.WriteLine("Qual o caminho que você deseja salvar o arquivo?");
+      string path = Console.ReadLine();
+
+      using (var file = new StreamWriter(path))
+      {
+        file.Write(text);
+      }
+
+      Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+      Menu();
+    }
+  }
 }
